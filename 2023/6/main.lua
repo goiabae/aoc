1af11b1b7ecaa095e8d2e473def6af44b461ce0f
goiabae <goiabae@protonmail.com>
2023-12-14 11:42:18 -0300

    add 2023 day 6 part 2 solution

--- 2023/6/main.lua
+++ 2023/6/main.lua
@@ -1,8 +1,8 @@
 require("aoc")
 
 local function parse(lines)
-	local time = split_on_spaces(lines[1]:sub(#"Time:", #lines[1])):map(tonumber)
-	local dist = split_on_spaces(lines[2]:sub(#"Distandce:", #lines[2])):map(tonumber)
+	local time = split_on_spaces(lines[1]:sub(#"Time:", #lines[1]))
+	local dist = split_on_spaces(lines[2]:sub(#"Distandce:", #lines[2]))
 
 	return { time = time, dist = dist }
 end
@@ -24,13 +24,25 @@ local function part1(f)
 
 	local winning = 1
 	for i = 1, #program.time do
-		local race = { time = program.time[i], dist = program.dist[i] }
+		local race = { time = tonumber(program.time[i]), dist = tonumber(program.dist[i]) }
 		winning = winning * count_wins(race)
 	end
 	return winning
 end
 
+local function part2(f)
+	local program = List.from_iter(io.lines(f)):apply(parse)
+	local race = {
+		time = tonumber(program.time:reduce(concat)),
+		dist = tonumber(program.dist:reduce(concat))
+	}
+
+	return count_wins(race)
+end
+
 test({
 		{ func = part1, input = "example", output = 288 },
-		{ func = part1, input = "input" }
+		{ func = part1, input = "input", output = 160816 },
+		{ func = part2, input = "example", output = 71503 },
+		{ func = part2, input = "input", output = 46561107 },
 })
