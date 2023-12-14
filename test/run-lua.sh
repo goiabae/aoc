a219cdb241310d9c36f57c11730f53e1348f41e2
goiabae <goiabae@protonmail.com>
2023-12-14 11:22:03 -0300

    fix redirection in lua test script

--- test/run-lua.sh
+++ test/run-lua.sh
@@ -2,5 +2,5 @@
 base=$(pwd)
 cd "${1}"
 LUA_CMD=lua
-which luajit 2> /dev/null && LUA_CMD=luajit
+which luajit > /dev/null && LUA_CMD=luajit
 LUA_PATH="${base}/lib/?.lua" exec $LUA_CMD "${1}/main.lua"
