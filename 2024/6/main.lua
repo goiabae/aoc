6422a90ef176e25713d4a0c4c7cfd83115a29447
goiabae <gm4ch4d0@gmail.com>
2024-12-07 11:11:02 -0300

    rewrite 2024 day 6 solution

--- 2024/6/main.lua
+++ 2024/6/main.lua
@@ -1,66 +1,91 @@
-local aoc = require("aoc")
+local function parse_grid()
+	local grid = {}
+	local x, y = 1, 1
+	local sx, sy -- start x and y
+	local dot_count = 0
 
-local function find_pieces(mat, max_height, max_width)
-	local obs = {}
-	local start = nil
-	for i = 1, max_height do
-		for j = 1, max_width do
-			local char = mat[i][j]
-			local h = max_height - i + 1
-			local w = j
+	for line in io.lines("input") do
+		grid[y] = grid[y] or {}
 
-			if char == '#' then
-				table.insert(obs, { h = h, w = w })
-			elseif char == '^' then
-				start = { h = h, w = w }
+		for c in line:gmatch(".") do
+			if c == "." then
+				dot_count = dot_count + 1
+			elseif c == "^" then
+				sx, sy = x, y
 			end
+
+			grid[y][x] = c
+			x = x + 1
 		end
-	end
-	return obs, start
-end
 
-local function in_bounds(guard, max_height, max_width)
-	return guard.h <= max_height and guard.h >= 0 and guard.w <= max_width and guard.w >= 0
-end
-
-local function obstructed(guard, obs, speed)
-	for obj in aoc.iter(obs) do
-		if ((guard.h + speed.h) == obj.h) and ((guard.w + speed.w) == obj.w) then
-			return true
-		end
-	end
-	return false
-end
-
-local function part1(filename)
-	local points = aoc.parse_char_mat(aoc.read_file(filename))
-	local max_height = #points
-	local max_width = #(points[1])
-	local obs, guard = find_pieces(points, max_height, max_width)
-	local speed = { h = 1, w = 0 }
-	local passed = {}
-
-	assert(guard ~= nil)
-
-	while in_bounds(guard, max_height, max_width) do
-		table.insert(passed, { h = guard.h, w = guard.w })
-		if obstructed(guard, obs, speed) then
-			if speed.h == 1 and speed.w == 0 then
-				speed = { h = 0, w = 1 }
-			elseif speed.h == 0 and speed.w == 1 then
-				speed = { h = -1, w = 0 }
-			elseif speed.h == -1 and speed.w == 0 then
-				speed = { h = 0, w = -1 }
-			elseif speed.h == 0 and speed.w == -1 then
-				speed = { h = 1, w = 0 }
-			end
-		end
-		guard = { h = guard.h + speed.h, w = guard.w + speed.w }
+		x, y = 1, y + 1
 	end
 
-	local uniq = aoc.unique(passed, function (a, b) return a.h == b.h and a.w == b.w end)
-	return #uniq - 1
+	return grid, sx, sy, y-1, dot_count
 end
 
-assert(part1("example") == 41)
-assert(part1("input") == 4964)
+local function walk(grid, sx, sy)
+	local visited = {}
+	local visited_count = 0
+
+	local x, y = sx, sy
+	local dx, dy = 0, -1
+
+	while true do
+		visited[x] = visited[x] or {}
+		visited_count = visited_count + (visited[x][y] and 0 or 1)
+		visited[x][y] = true
+
+		local ny = y + dy
+		local nx = x + dx
+
+		if not (grid[ny] and grid[ny][nx]) then
+			return visited, visited_count
+		end
+
+		if grid[ny][nx] == "#" then
+			dx, dy = -dy, dx
+		else
+			x, y = nx, ny
+		end
+	end
+end
+
+local function loops(grid, sx, sy, bx, by, dot_count)
+	local x, y = sx, sy
+	local dx, dy = 0, -1
+
+	-- works for this particular input, might not work for all inputs?
+	for _ = 0, dot_count / 2 do
+		local ny = y + dy
+		local nx = x + dx
+
+		if not (grid[ny] and grid[ny][nx]) then
+			return false
+		end
+
+		-- next is a "#" or "O"
+		if grid[ny][nx] == "#" or ((nx == bx) and (ny == by)) then
+			dx, dy = -dy, dx
+		else
+			x, y = nx, ny
+		end
+	end
+
+	return true
+end
+
+local grid, sx, sy, side_length, dot_count = parse_grid()
+local path, path_length = walk(grid, sx, sy)
+
+local loop_count = 0
+for x = 1, side_length do
+	for y = 1, side_length do
+		if path[x] and path[x][y] and loops(grid, sx, sy, x, y, dot_count) then
+			loop_count = loop_count + 1
+		end
+	end
+end
+
+assert(path_length == 4964)
+assert(loop_count == 1740)
