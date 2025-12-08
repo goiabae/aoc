c85e5cdee50041d5532eabfb6a36811d1c027bad
goiabae <gm4ch4d0@gmail.com>
2025-12-08 19:34:30 -0300

    update 2023 day 6

--- 2023/6/main.lua
+++ 2023/6/main.lua
@@ -1,48 +1,38 @@
-require("aoc")
+local aoc = require("aoc")
 
 local function parse(lines)
-	local time = split_on_spaces(lines[1]:sub(#"Time:", #lines[1]))
-	local dist = split_on_spaces(lines[2]:sub(#"Distandce:", #lines[2]))
-
+	local time = aoc.split_with(lines[1]:sub(#"Time: ", #lines[1]), " ")
+	local dist = aoc.split_with(lines[2]:sub(#"Distandce:", #lines[2]), " ")
 	return { time = time, dist = dist }
 end
 
-local function count_wins(race)
+---@param time integer
+---@param dist integer
+local function count_wins(time, dist)
 	local wins = 0
-
-	for t = 0, race.time do
-		if ((race.time - t) * t) > race.dist then
+	for t = 2, time do
+		if ((time - t) * t) > dist then
 			wins = wins + 1
 		end
 	end
-
 	return wins
 end
 
-local function part1(f)
-	local program = List.from_iter(io.lines(f)):apply(parse)
-
+---@type solver
+local function solve (filename)
+	local program = parse(aoc.collect(io.lines(filename)))
 	local winning = 1
 	for i = 1, #program.time do
-		local race = { time = tonumber(program.time[i]), dist = tonumber(program.dist[i]) }
-		winning = winning * count_wins(race)
+		local time = tonumber(program.time[i])
+		local dist = tonumber(program.dist[i])
+		winning = winning * count_wins(time, dist)
 	end
-	return winning
+	local p1 = winning
+	local time = tonumber(aoc.list.reduce(program.time, aoc.concat))
+	local dist = tonumber(aoc.list.reduce(program.dist, aoc.concat))
+	local p2 = count_wins(time, dist)
+	return p1, p2
 end
 
-local function part2(f)
-	local program = List.from_iter(io.lines(f)):apply(parse)
-	local race = {
-		time = tonumber(program.time:reduce(concat)),
-		dist = tonumber(program.dist:reduce(concat))
-	}
-
-	return count_wins(race)
-end
-
-test({
-		{ func = part1, input = "example", output = 288 },
-		{ func = part1, input = "input", output = 160816 },
-		{ func = part2, input = "example", output = 71503 },
-		{ func = part2, input = "input", output = 46561107 },
-})
+aoc.verify(solve, "example", 288, 71503)
+aoc.verify(solve, "input", 160816, 46561107)
