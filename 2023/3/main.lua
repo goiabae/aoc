fcd964e3e3bdd2675b13e7775da3c02dbbd36d90
goiabae <gm4ch4d0@gmail.com>
2025-12-08 02:56:30 -0300

    update 2023 day 3

--- 2023/3/main.lua
+++ 2023/3/main.lua
@@ -1,141 +1,103 @@
-require("aoc")
+local aoc = require("aoc")
 
+---@param char string?
+---@return boolean
 local function is_symbol(char)
-	return char ~= '.' and not is_digit(char) and char ~= nil
+	return char and char ~= '.' and not aoc.is_digit(char) or false
 end
 
-local function has_adjacent_symbol(lines, cell)
-	return List.from_list({
-		{-1, -1}, {-1, 0}, {-1, 1},
-		{ 0, -1},          { 0, 1},
-		{ 1, -1}, { 1, 0}, { 1, 1}
-	})
-		:map(function(x) return is_symbol(lines:at(cell + x)) end)
-		:reduce(log_or)
-end
-
+---@param mat string[][]
 local function adjacent(mat, num)
-	local adj = List.from_list({
-		{ num.i-1, num.j.from-1 },
-		{ num.i  , num.j.from-1 },
-		{ num.i+1, num.j.from-1 },
-		{ num.i-1, num.j.to+1   },
-		{ num.i  , num.j.to+1   },
-		{ num.i+1, num.j.to+1   }
-	})
+	local adj = {
+		{ num.i-1, num.from-1 },
+		{ num.i  , num.from-1 },
+		{ num.i+1, num.from-1 },
+		{ num.i-1, num.to+1   },
+		{ num.i  , num.to+1   },
+		{ num.i+1, num.to+1   }
+	}
 
-	for j = num.j.from, num.j.to do
+	for j = num.from, num.to do
 		table.insert(adj, { num.i-1, j })
 		table.insert(adj, { num.i+1, j })
 	end
 
-	adj = adj:filter(function(coord) return is_symbol(mat:at(coord)) end)
-	return { num = num, adj = adj }
+	return aoc.list.filter(adj, function(coord) return is_symbol(aoc.matrix_at(mat, coord)) end)
 end
 
-local function find_nums(mat)
-	local nums = List()
-	for i = 1, #mat do
-		local j = 1
-		while j <= #(mat[i]) do
-			if is_digit(mat[i][j]) then
-				local k = j
-				local num = { i = i, j = { from = j, to = j } }
-				while is_digit(mat[i][k+1]) do
-					num.j.to = num.j.to + 1
-					k = k + 1
-				end
-				j = k + 1
-				table.insert(nums, num)
-			else
-				j = j + 1
-			end
+---@param filename string
+---@return { i: integer, from: integer, to: integer }[]
+local function find_nums(filename)
+	local nums = {}
+	local i = 1
+	for line in io.lines(filename) do
+		local s, e, _ = string.find(line, "[%d]+", 1)
+		while s do
+			table.insert(nums, { i = i, from = s, to = e })
+			s, e, _ = string.find(line, "[%d]+", e + 1)
 		end
+		i = i + 1
 	end
-
 	return nums
 end
 
-local function find_parts(mat)
-	local function is_part(num)
-		for j = num.j.from, num.j.to do
-			if has_adjacent_symbol(mat, List.from_list({ num.i, j })) then
-				return true
-			end
-		end
-		return false
+---@return integer
+local function to_number(mat, row, from, to)
+	local s = ""
+	for i = from, to do
+		s = s .. mat[row][i]
 	end
-
-	return find_nums(mat)
-		:filter(is_part)
-		:map(
-			function(part)
-				return tonumber(reduce(concat, slice(mat[part.i], part.j.from, part.j.to)))
-			end
-		)
+	return assert(tonumber(s))
 end
 
-local function find_gears(mat)
-	local function to_number(num)
-		return tonumber(reduce(concat, slice(mat[num.i], num.j.from, num.j.to)))
-	end
+---@param mat string[][]
+local function sum_parts(mat, nums)
+	return aoc.list.sum(nums, function (_, part)
+		for j = part.from, part.to do
+			if aoc.exists_adjacent(mat, part.i, j, is_symbol) then
+				return to_number(mat, part.i, part.from, part.to)
+			end
+		end
+		return 0
+	end)
+end
 
-	local num_adjs = find_nums(mat):map(function(num) return adjacent(mat, num) end)
+---@param mat string[][]
+---@return integer
+local function sum_gears(mat, nums)
 
 	local gears = {}
 
-	for i = 1, #num_adjs do
-		local num = num_adjs[i].num
-
-		for j = 1, #(num_adjs[i].adj) do
-			local adj = num_adjs[i].adj[j]
-
-			if not gears[adj[1]] then
-				gears[adj[1]] = {}
-			end
-
-			if not gears[adj[1]][adj[2]] then
-				gears[adj[1]][adj[2]] = {}
-			end
-
-			table.insert(gears[adj[1]][adj[2]], to_number(num))
+	for num in aoc.list.iter(nums) do
+		for adj in aoc.list.iter(adjacent(mat, num)) do
+			local a = adj[1]
+			local b = adj[2]
+			gears[a] = gears[a] or {}
+			gears[a][b] = gears[a][b] or {}
+			table.insert(gears[a][b], to_number(mat, num.i, num.from, num.to))
 		end
 	end
 
-	local res = List()
+	local s = 0
 
-	for i, _ in pairs(gears) do
-		for j, v in pairs(gears[i]) do
-			table.insert(res, { coord = { i, j }, parts = v })
+	for _, x in pairs(gears) do
+		for _, v in pairs(x) do
+			if #v == 2 then
+				s = s + v[1] * v[2]
+			end
 		end
 	end
 
-	return res
+	return s
 end
 
-local function part1(f)
-	return List
-		.from_iter(io.lines(f))
-		:map(split_chars)
-		:apply(find_parts)
-		:reduce(plus)
+---@type solver
+local function solve (filename)
+	local b = aoc.parse_char_mat(aoc.read_file(filename))
+	local nums = find_nums(filename)
+	return sum_parts(b, nums), sum_gears(b, nums)
 end
 
-local function part2(f)
-	return List
-		.from_iter(io.lines(f))
-		:map(split_chars)
-		:apply(find_gears)
-		:get("parts")
-		:filter(function(parts) return #parts == 2 end)
-		:map(function(parts) return parts[1] * parts[2] end)
-		:reduce(plus)
-end
-
-test({
-	{ func = part1, input = 'example',   output = 4361   },
-	{ func = part1, input = 'test1', output = 13112  },
-	{ func = part1, input = "input", output = 543867 },
-	{ func = part2, input = "example"  , output = 467835 },
-	{ func = part2, input = "input", output = 79613331 },
-})
+aoc.verify(solve, "example", 4361, 467835)
+aoc.verify(solve, "test1", 13112, 7435797)
+aoc.verify(solve, "input", 543867, 79613331)
