d8890ee14340667c0702ce7906d2fbbc951c837d
goiabae <gm4ch4d0@gmail.com>
2025-12-08 03:41:04 -0300

    update 2023 day 4

--- 2023/4/main.lua
+++ 2023/4/main.lua
@@ -1,62 +1,42 @@
-require("aoc")
+local aoc = require("aoc")
 
-local function parse(str)
-	local colon = find_char(":", str)
-	local pipe = find_char("|", str)
+local function winning_nums(str)
+	local colon = aoc.find_char(":", str)
+	local pipe = aoc.find_char("|", str)
 
-	return List.from_table({
-		winning = split_on_spaces(str:sub(colon+2, pipe-2)):map(tonumber),
-		scratch = split_on_spaces(str:sub(pipe+2, #str)):map(tonumber)
-	})
-end
+	local winning = aoc.split_with(str:sub(colon+2, pipe-2), " ", tonumber)
+	local scratch = aoc.split_with(str:sub(pipe+2, #str), " ", tonumber)
 
-local function winning_nums(game)
-	local present = List()
-	for i = 1, #game.winning do
-		for j = 1, #game.scratch do
-			if game.scratch[j] == game.winning[i] then
-				table.insert(present, game.winning[i])
+	local s = 0
+	for i = 1, #winning do
+		for j = 1, #scratch do
+			if scratch[j] == winning[i] then
+				s = s + 1
 			end
 		end
 	end
-	return present
+	return s
 end
 
-local function pow(b, e)
-	local acc = 1
-	for _ = 1, e do
-		acc = acc * b
-	end
-	return acc
-end
+---@type solver
+local function solve (filename)
+	local wins = aoc.list.map(aoc.collect(io.lines(filename)), winning_nums)
 
-local function part1(f)
-	return List
-		.from_iter(io.lines(f))
-		:map(parse)
-		:map(winning_nums)
-		:map(function(nums) return #nums end)
-		:map(function(count) if count > 0 then return pow(2, count-1) else return 0 end end)
-		:reduce(plus)
-end
+	local p1 = aoc.list.sum(wins, function(_, count)
+		if count > 0 then return 2 ^ (count-1) else return 0 end
+	end)
 
-local function part2(f)
-	local games = List.from_iter(io.lines(f)):map(parse):map(function(game) game.count = 1; return game end)
-	local total = 0
-	for i = 1, #games do
-		total = total + games[i].count
-		local won = #(winning_nums(games[i]))
-		for j = 1, won do
-			if (i + j) > #games then break end
-			games[i+j].count = games[i+j].count + games[i].count
+	local counts = aoc.list.init(aoc.len(wins), 1)
+	local p2 = 0
+	for i = 1, #wins do
+		p2 = p2 + counts[i]
+		for j = 1, math.min(wins[i], #wins - i) do
+			counts[i+j] = counts[i+j] + counts[i]
 		end
 	end
-	return total
+
+	return p1, p2
 end
 
-test({
-	{ func = part1, input = 'example', output = 13 },
-	{ func = part1, input = "input",   output = 26218 },
-	{ func = part2, input = "example", output = 30 },
-	{ func = part2, input = "input",   output = 9997537 },
-})
+aoc.verify(solve, "example", 13, 30)
+aoc.verify(solve, "input", 26218, 9997537)
