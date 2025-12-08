188addf139f3263227f9f48b526e013cb09f4817
goiabae <gm4ch4d0@gmail.com>
2025-12-08 19:04:11 -0300

    update 2023 day 5

--- 2023/5/main.lua
+++ 2023/5/main.lua
@@ -1,120 +1,57 @@
-require("aoc")
+local aoc = require("aoc")
 
 local function parse(lines)
-	local seeds = split_on_spaces(lines[1]:sub(find_char(" ", lines[1])+1, #(lines[1]))):map(tonumber)
-
-	local i = 3
-	local maps = List()
+	local seeds = aoc.split_with(lines[1]:sub(#"seeds: "+1), " ", tonumber)
+	local i = 2
+	local maps = {}
 	while i <= #lines do
-		if lines[i] ~= "" then
-			local dash = find_char("-", lines[i])
-			local space = find_char(" ", lines[i])
-			local map = {
-				from = lines[i]:sub(1, dash-1),
-				to = lines[i]:sub(dash + #"to-" + 1, space-1),
-				ranges = {}
-			}
-
-			local k = 1
-			while lines[i+k] ~= "" and (i+k) <= #lines do
-				table.insert(map.ranges, split_on_spaces(lines[i+k]):map(tonumber))
-				k = k + 1
-			end
-			table.insert(maps, map)
-			i = i + k
+		if lines[i] == "" then
+			i = i + 1
 		end
+
+		local map = {}
+		local k = 1
+		while (i+k) <= #lines and lines[i+k] ~= "" do
+			local ns = aoc.split_with(lines[i+k], " ", tonumber)
+			table.insert(map, ns)
+			k = k + 1
+		end
+		table.insert(maps, map)
+		i = i + k
 		i = i + 1
 	end
-
 	return { seeds = seeds, maps = maps }
 end
 
-local function compile(input)
-	local str = ""
-	str = str .. "seeds: " .. input.seeds:reduce(function(x, y) return x .. " " .. y end) .. "\n"
-
-	for i = 1, #input.maps do
-		local map = input.maps[i]
-		str = str .. "\n"
-		str = str .. map.from .. "-to-" .. map.to .. " map:" .. "\n"
-		for j = 1, #map.ranges do
-			str = str .. map.ranges[j]:reduce(function(x, y) return x .. " " .. y end) .. "\n"
-		end
-	end
-
-	return str
-end
-
 local function convert(num, map)
-	for i = 1, #map.ranges do
-		local range = map.ranges[i]
+	for range in aoc.list.iter(map) do
 		local dest = range[1]
 		local source = range[2]
 		local length = range[3]
-
-		if num >= source and num <= (source + length) then
+		if source <= num and num <= (source + length) then
 			local offset = num - source
 			return dest + offset
 		end
 	end
-
 	return num
 end
 
-local function part1(f)
-	local input = read_file(f)
-	local almanac = List.from_iter(io.lines(f)):apply(parse)
-	local compiled = compile(almanac)
-
-	if input ~= compiled then
-		print(input)
-		print(compiled)
-	end
-	assert(input == compiled)
-
+---@type solver
+local function solve (filename)
+	local almanac = parse(aoc.collect(io.lines(filename)))
 	local acc = almanac.seeds
-
+	almanac.seeds = aoc.group(almanac.seeds, 2)
+	local ranges = aoc.list.map(almanac.seeds, function (s) return { s[1], s[1]+s[2]-1 } end)
+	local set = aoc.range_set.from_range_list(ranges)
 	for i = 1, #almanac.maps do
 		local map = almanac.maps[i]
-		acc = acc:map(function(num) return convert(num, map) end)
+		set = aoc.range_set.slide_transform(set, map)
+		acc = aoc.list.map(acc, function(num) return convert(num, map) end)
 	end
-
-	return acc:reduce(min)
+	local p1 = aoc.list.reduce(acc, math.min)
+	local p2 = aoc.range_set.min(set)
+	return p1, p2
 end
 
--- FIXME brute force approach
-local function part2(f)
-	local input = read_file(f)
-	local almanac = List.from_iter(io.lines(f)):apply(parse)
-	local compiled = compile(almanac)
-
-	if input ~= compiled then
-		print(input)
-		print(compiled)
-	end
-	assert(input == compiled)
-
-	almanac.seeds = almanac.seeds:group(2)
-
-	local min = math.huge
-
-	for range in almanac.seeds:iter() do
-		for num = range[1], range[1] + range[2] - 1 do
-			local acc = num
-			for map in almanac.maps:iter() do
-				acc = convert(acc, map)
-			end
-			if acc < min then
-				min = acc
-			end
-		end
-	end
-
-	return min
-end
-
-test({
-	{ func = part1, input = "example", output = 35 },
-	{ func = part1, input = "input", output = 26273516 },
-	{ func = part2, input = "input", output = 34039469 }
-})
+aoc.verify(solve, "example", 35, 46)
+aoc.verify(solve, "input", 26273516, 34039469)
